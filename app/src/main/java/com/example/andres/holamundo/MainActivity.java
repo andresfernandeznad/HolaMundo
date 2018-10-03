e891dae895ae549d3a9aeedb878f586fef9ffe79
andresfernandeznad <andresfernandeznadales@gmail.com>
2018-10-03 10:56:34 +0200

    Segundo commit

--- app/src/main/java/com/example/andres/holamundo/MainActivity.java
+++ app/src/main/java/com/example/andres/holamundo/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.andres.holamundo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tv;
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = (TextView) findViewById(R.id.textView);
+        btn = (Button) findViewById(R.id.button3);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tv.setText("Has pulsado el botón");
+            }
+        });
     }
 }
